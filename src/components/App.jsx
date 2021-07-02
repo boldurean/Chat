ffacd30f2849d187d96710cbe0817a4e190b903d
vsbdn <v.boldurean@gmail.com>
2021-07-02 15:48:31 +0100

    fix: remove container class from root div

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -59,7 +59,7 @@ const App = () => (
   <AuthProvider>
     <Router>
       <div className="d-flex flex-column h-100">
-        <Navbar bsPrefix="shadow-sm navbar navbar-light bg-white mb-2" expand="true">
+        <Navbar bsPrefix="shadow-sm navbar navbar-light bg-white mb-2 px-2" expand="true">
           <Navbar.Brand as={Link} to="/">Hexlet Chat</Navbar.Brand>
           <AuthButton />
         </Navbar>
