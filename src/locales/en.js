35010c2c9992e3b40bab6499833ae7d444b31d11
vsbdn <v.boldurean@gmail.com>
2021-07-06 09:57:33 +0100

    Add: localization

---
+++ src/locales/en.js
@@ -0,0 +1,53 @@
+export default {
+  translation: {
+    languages: {
+      en: 'English',
+      ru: 'Русский',
+    },
+    buttons: {
+      login: 'Log in',
+      logout: 'Log out',
+      signup: 'Sign Up',
+      add: 'Add',
+      cancel: 'Cancel',
+      rename: 'Rename',
+      remove: 'Delete',
+      send: 'Send',
+    },
+    forms: {
+      nickname: 'Nickname',
+      username: 'Username',
+      password: 'Password',
+      passwordConfirmation: 'Confirm password',
+    },
+    errors: {
+      required: 'Required field',
+      fromTo: 'From {{min}} to {{max}} symbols',
+      from: 'From {{min}} symbols',
+      matchPassword: 'Passwords mush match',
+      userExists: 'User with this name already exists',
+      notOneOf: 'Channel with this name already exists',
+      auth: 'Incorrect user or password',
+    },
+    texts: {
+      login: 'Login',
+      register: 'Register',
+      'do not have an account?': "Don't have account?",
+    },
+    channels: {
+      channels: 'Channels',
+      addNew: 'Add new channel',
+      remove: 'Remove channel',
+      rename: 'Rename channel',
+      removeConfirmation: 'Are you sure you want to delete this channel?',
+      channelName: 'Channel name',
+    },
+    messages: {
+      counter: {
+        key_plural: '{{count}} messages',
+        key: '{{count}} message',
+      },
+      enterMessage: 'Enter message',
+    },
+  },
+};
---
+++ src/locales/ru.js
@@ -0,0 +1,54 @@
+export default {
+  translation: {
+    languages: {
+      en: 'English',
+      ru: 'Русский',
+    },
+    buttons: {
+      login: 'Войти',
+      logout: 'Выйти',
+      signup: 'Зарегистрироваться',
+      add: 'Добавить',
+      cancel: 'Отменить',
+      rename: 'Переименовать',
+      remove: 'Удалить',
+      send: 'Отправить',
+    },
+    forms: {
+      nickname: 'Ваш ник',
+      username: 'Имя пользователя',
+      password: 'Пароль',
+      passwordConfirmation: 'Подтвердите пароль',
+    },
+    errors: {
+      required: 'Обязательное поле',
+      fromTo: 'От {{min}} до {{max}} символов',
+      from: 'От {{min}} символов',
+      matchPassword: 'Пароли должны совпадать',
+      userExists: 'Пользователь с таким именем существует',
+      notOneOf: 'Канал с таким названием уже существует',
+      auth: 'Неверное имя пользователя или пароль',
+    },
+    texts: {
+      login: 'Войти',
+      register: 'Регистрация',
+      'do not have an account?': 'Нет аккаунта?',
+    },
+    channels: {
+      channels: 'Каналы',
+      addNew: 'Добавить новый канал',
+      remove: 'Удалить канал',
+      rename: 'Переименовать канал',
+      removeConfirmation: 'Вы уверены, что хотите удалить этот канал?',
+      channelName: 'Название',
+    },
+    messages: {
+      counter: {
+        key_0: '{{count}} сообщение',
+        key_1: '{{count}} сообщения',
+        key_2: '{{count}} сообщений',
+      },
+      enterMessage: 'Введите сообщение',
+    },
+  },
+};
