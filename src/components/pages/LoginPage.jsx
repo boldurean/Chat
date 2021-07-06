6b0d3db6f83af22259998b1536c18176c3f3d1a0
vsbdn <v.boldurean@gmail.com>
2021-07-06 12:57:05 +0100

    test: add rollbar + auth test

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -43,6 +43,8 @@ const LoginPage = () => {
         if (err.isAxiosError && err.response.status === 401) {
           setAuthFailed(true);
           inputRef.current.select();
+          rollbar.error(err);
+          rollbar.log('test err');
           return;
         }
         rollbar.error(err);
