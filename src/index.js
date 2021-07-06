eded7a57b00325375036a0d64533f2b878c02dd9
vsbdn <v.boldurean@gmail.com>
2021-07-06 14:58:52 +0100

    Fix entry pooint

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@
 import 'core-js/stable/index.js';
 import 'regenerator-runtime/runtime.js';
 import '../assets/application.scss';
-import React from 'react';
-import { render } from 'react-dom';
-import App from './init.jsx';
+import runApp from './init.jsx';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
-render(<App />, document.getElementById('chat'));
+runApp();
 
 console.log('it works!');
--- src/init.jsx
+++ src/init.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { render } from 'react-dom';
 import { I18nextProvider } from 'react-i18next';
 import { Provider } from 'react-redux';
 import E from './api/events.js';
@@ -8,40 +9,41 @@ import i18n from './locales';
 import { actions } from './slices/index.js';
 import createStore from './store.js';
 
-const store = createStore();
+export default () => {
+  const store = createStore();
 
-const {
-  newMessage, newChannel, removeChannel, renameChannel, clearMessages,
-} = actions;
+  const {
+    newMessage, newChannel, removeChannel, renameChannel, clearMessages,
+  } = actions;
 
-socket.on(E.NEW_MESSAGE, (data) => {
-  store.dispatch(newMessage(data));
-});
+  socket.on(E.NEW_MESSAGE, (data) => {
+    store.dispatch(newMessage(data));
+  });
 
-socket.on(E.DELETE_MESSAGES, (data) => {
-  console.log(data);
-  store.dispatch(clearMessages());
-});
+  socket.on(E.DELETE_MESSAGES, (data) => {
+    console.log(data);
+    store.dispatch(clearMessages());
+  });
 
-socket.on(E.NEW_CHANNEL, (data) => {
-  console.log(data);
-  store.dispatch(newChannel(data));
-});
+  socket.on(E.NEW_CHANNEL, (data) => {
+    console.log(data);
+    store.dispatch(newChannel(data));
+  });
 
-socket.on(E.REMOVE_CHANNEL, (data) => {
-  store.dispatch(removeChannel(data));
-});
+  socket.on(E.REMOVE_CHANNEL, (data) => {
+    store.dispatch(removeChannel(data));
+  });
 
-socket.on(E.RENAME_CHANNEL, (data) => {
-  store.dispatch(renameChannel(data));
-});
+  socket.on(E.RENAME_CHANNEL, (data) => {
+    store.dispatch(renameChannel(data));
+  });
 
-const init = () => (
-  <I18nextProvider i18n={i18n}>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </I18nextProvider>
-);
-
-export default init;
+  render(
+    <I18nextProvider i18n={i18n}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </I18nextProvider>,
+    document.getElementById('chat'),
+  );
+};
