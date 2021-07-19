6fcb290220fd8f4daa5405dddc5bfd36213ccb7e
vsbdn <v.boldurean@gmail.com>
2021-07-19 18:51:24 +0100

    add: add scroll to the bottom on messaging

--- src/features/chat/components/MessagesBox.jsx
+++ src/features/chat/components/MessagesBox.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import { messageSelectors } from '../Slice.js';
 
 const MessagesBox = () => {
   const currentChannelMessages = useSelector(messageSelectors.getCurrentChannelMessages);
 
+  const messageBoxRef = useRef();
+
+  const scrollToBottom = (node) => {
+    // eslint-disable-next-line no-param-reassign
+    node.scrollTop = node.scrollHeight;
+  };
+
+  useEffect(() => {
+    scrollToBottom(messageBoxRef.current);
+  }, [currentChannelMessages]);
+
   return (
-    <div id="message-box" className="chat-messages overflow-auto px-5 ">
+    <div ref={messageBoxRef} id="message-box" className="chat-messages overflow-auto px-5 ">
       {currentChannelMessages
         && currentChannelMessages.map((msg) => (
           <div key={msg.id} className="text-break mb-2">
