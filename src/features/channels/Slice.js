32a199500ab5f2174fa5e6859435954fabc63335
vsbdn <v.boldurean@gmail.com>
2021-07-27 18:47:07 +0100

    fix svg imports/ scroll to the sent message/ code refact

--- src/features/channels/Slice.js
+++ src/features/channels/Slice.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-param-reassign */
 import { createSelector, createSlice } from '@reduxjs/toolkit';
-import { first } from 'lodash';
+import first from 'lodash/first';
 import fetchData from '../init.js';
 
 export const slice = createSlice({
